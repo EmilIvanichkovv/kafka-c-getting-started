383ad68574d8a40e38d636035c44f1609a3dc40b
Emil <emil.ivanichkov@gmail.com>
2022-04-13 13:55:19 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Kafka C - Getting Started";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "kafka-c-getting-started";
+      shell = ./shell.nix;
+    };
+}
---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> { } }: with pkgs;
+
+# let
+#   myglib = callPackage nix/pkgs/glib/default.nix { util-linuxMinimal = glib; };
+# in
+mkShell {
+
+  buildInputs = [
+    nix-prefetch-git
+    figlet
+    gcc
+    pkg-config
+    rdkafka
+    glib
+  ];
+  shellHook = ''
+    figlet "Welcome to Kafka C - Getting Started"
+    echo $LIBCLANG_PATH
+
+  '';
+}
