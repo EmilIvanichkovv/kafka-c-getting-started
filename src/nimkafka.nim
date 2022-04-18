d377cce024ec346820df99ded6b9dbb8f72578ec
Emil <emil.ivanichkov@gmail.com>
2022-04-18 15:57:25 +0300

    This one worked for cpp

--- src/nimkafka.nim
+++ src/nimkafka.nim
@@ -1,4 +1,6 @@
 
+{.link: "/nix/store/zwbf01pncrclfvlgrkwgmfwyhbb0zcnf-rdkafka-1.8.2/lib/librdkafka++.so"}
+
 const
   librdkafka* = "librdkafka++.so"
   irr = "<librdkafka/rdkafkacpp.h>"
@@ -6,9 +8,11 @@ const
 # header: irr,
 
 # RdKafka::version,
-proc rd_kafka_version_cpp*(): int {.cdecl,
+proc rd_kafka_version_cpp*(): int {.
+                                    header: irr,
                                     importcpp: "RdKafka::version()",
-                                    dynlib: librdkafka.} ## Returns the librdkafka version as string.
+                                    # dynlib: librdkafka.
+                                    } ## Returns the librdkafka version as string.
 
 
 
