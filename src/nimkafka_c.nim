bd8d26ebc4f34e7a39a94ddf0a85820217c0e2e6
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-04-19 22:43:01 +0300

    feat(nim_rdkafka_wrapper): Extend

--- src/nimkafka_c.nim
+++ src/nimkafka_c.nim
@@ -1,7 +1,22 @@
 const
   rdkafkadll* = "librdkafka.so"
 
+type
+  RDKConf* = object
+  PRDKConf* = ptr RDKConf
+  RDKConfRes* {.size: sizeof(cint).} = enum
+    ##Configuration result type
+    RD_KAFKA_CONF_UNKNOWN = - 2, ##Unknown configuration name. 
+    RD_KAFKA_CONF_INVALID = - 1, ##Invalid configuration value. 
+    RD_KAFKA_CONF_OK = 0
+
 proc rd_kafka_version_c*(): cstring {.cdecl, importc: "rd_kafka_version_str",
                                       dynlib: rdkafkadll.}
 
-echo rd_kafka_version_c()
+
+proc rd_kafka_conf_set*(conf: PRDKConf; name: cstring; value: cstring;
+                       errstr: cstring; errstr_size: csize): RDKConfRes {.
+    cdecl, importc: "rd_kafka_conf_set", dynlib: rdkafkadll.} 
+
+proc rd_kafka_conf_new*(): PRDKConf {.cdecl, importc: "rd_kafka_conf_new",
+    dynlib: rdkafkadll.} 
