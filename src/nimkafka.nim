c631f9910e27e6a5e7db25f873b166882c379768
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-04-19 22:41:04 +0300

    feat(nim_rdkafkacpp_wrapper): Fixes. Add dump

--- src/nimkafka.nim
+++ src/nimkafka.nim
@@ -7,6 +7,16 @@ const
   librdkafka* = "librdkafka++.so"
   irr = "<librdkafka/rdkafkacpp.h>"
 
+type
+  List*[T]                              {.importcpp: "std::list", header: "<list>".} = object
+  ListIter*[T]                          {.importcpp: "std::list<'0>::iterator", header: "<list>".} = object
+
+proc initList*[T](): List[T]            {.importcpp: "std::list<'*0>()", constructor, header: "<list>".}
+proc size*(l: List): csize_t            {.importcpp: "size", header: "<list>".}
+proc begin*[T](l: List[T]): ListIter[T] {.importcpp: "begin", header: "<list>".}
+proc `[]`*[T](it: ListIter[T]): T       {.importcpp: "*#", header: "<list>".}
+
+
 # VERSION
 
 proc rd_kafka_version_cpp*(): int {.
@@ -18,20 +28,24 @@ proc rd_kafka_version_cpp*(): int {.
 # CONF Utils:
 
 type
+
   Conf* {.header: irr,
          importcpp: "RdKafka::Conf".} = object
-
-  ConfType* {.header: irr,
-             importcpp: "RdKafka::Conf::ConfType".} = enum
+  PConf* = ptr Conf
+  ConfType* = enum
     CONF_GLOBAL, CONF_TOPIC
 
-  ConfResult* {.header: irr,
-               importcpp: "RdKafka::Conf::ConfResult".} = enum
-    CONF_UNKNOWN, CONF_INVALID, CONF_OK
+  ConfResult* = enum
+    CONF_UNKNOWN = - 2,
+    CONF_INVALID = - 1,
+    CONF_OK = 0
 
-proc create* (confType: ConfType):ref Conf
+proc create* (confType: ConfType):ptr Conf
   {.header: irr, importcpp: "RdKafka::Conf::create(@)".}
 
 
-proc set* (this: Conf, name, value: CppString, errstr: var CppString ): ConfResult
+proc set* (this: PConf, name, value: CppString, errstr: var CppString ): ConfResult
   {.header: irr, importcpp: "#.set(@)".}
+
+proc dump*(this: PConf): ref List[CppString]
+ {.header: irr, importcpp: "#.dump()".}
