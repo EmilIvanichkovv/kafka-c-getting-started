0320be336c109fa472b8afd91fdf98166550b2b5
Emil <emil.ivanichkov@gmail.com>
2022-04-19 16:25:00 +0300

    feat(nim_producer): Some test lines

--- src/producer.nim
+++ src/producer.nim
@@ -1,3 +1,21 @@
 import ./nimkafka
+import cppstl
 
 echo rd_kafka_version_cpp()
+
+var a: ptr Conf
+echo ConfType.CONF_GLOBAL
+var b = create(ConfType.CONF_GLOBAL)
+echo b.typeof
+echo b[].typeof
+
+
+let
+  name = "bootstrap.servers".initCppString
+  broker = "localhost:9092".initCppString
+var str = initCppString()
+
+let res = set(b[], name, broker, str)
+echo res
+echo $str
+
