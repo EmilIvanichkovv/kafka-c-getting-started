650a3a5fdd7f99fc5e6257971755993084ced5e1
Emil <emil.ivanichkov@gmail.com>
2022-04-19 16:21:07 +0300

    feat(nim_rdkafkacpp_wrapper): Fix `set` proc
    for `Conf`. Use `cppstl` lib

--- src/nimkafka.nim
+++ src/nimkafka.nim
@@ -1,5 +1,8 @@
 
 {.link: "/nix/store/zwbf01pncrclfvlgrkwgmfwyhbb0zcnf-rdkafka-1.8.2/lib/librdkafka++.so"}
+
+import cppstl
+
 const
   librdkafka* = "librdkafka++.so"
   irr = "<librdkafka/rdkafkacpp.h>"
@@ -15,37 +18,20 @@ proc rd_kafka_version_cpp*(): int {.
 # CONF Utils:
 
 type
-  Conf {.header: irr,
+  Conf* {.header: irr,
          importcpp: "RdKafka::Conf".} = object
 
-  ConfType {.header: irr,
-            importcpp: "RdKafka::Conf::ConfType".} = enum
+  ConfType* {.header: irr,
+             importcpp: "RdKafka::Conf::ConfType".} = enum
     CONF_GLOBAL, CONF_TOPIC
 
-  ConfResult{.header: irr,
-              importcpp: "RdKafka::Conf::ConfResult".} = enum
+  ConfResult* {.header: irr,
+               importcpp: "RdKafka::Conf::ConfResult".} = enum
     CONF_UNKNOWN, CONF_INVALID, CONF_OK
 
-proc create(confType: ConfType):ref Conf
+proc create* (confType: ConfType):ref Conf
   {.header: irr, importcpp: "RdKafka::Conf::create(@)".}
 
 
-proc set(this: Conf, name, value, errstr: cstring): ConfResult
- {.header: irr, importcpp: "#.RdKafka::Conf::set(@)".}
-
-
-
-echo rd_kafka_version_cpp()
-
-var a: ptr Conf
-echo ConfType.CONF_GLOBAL
-var b = create(ConfType.CONF_GLOBAL)
-echo b.typeof
-echo b[].typeof
-const
-  broker: cstring = "localhost:9092"
-  topic: cstring = "purchase"
-
-var err:ref cstring
-let res = set(a[], broker, topic, err[])
-# echo res
+proc set* (this: Conf, name, value: CppString, errstr: var CppString ): ConfResult
+  {.header: irr, importcpp: "#.set(@)".}
