9f6042192b7a532eaa76e8f4dd39720c51a7a7b3
Emil <emil.ivanichkov@gmail.com>
2022-05-03 19:02:01 +0300

    feat(nim_avro_example): More test lines

--- src/avro_example.nim
+++ src/avro_example.nim
@@ -18,15 +18,17 @@ let otherChema =  "{\"type\":\"record\","&
 var err: string
 var idCounter:int64
 
-# var testSchema: AvroSchema_t
+var
+  testObj: AvroObjT
+  testSchema = testObj.addr
 
-# let jsonSchema = avroSchemaFromJsonLength(otherChema.cstring, otherChema.len+1, testSchema.addr)
+# let jsonSchema = avroSchemaFromJsonLength(PERSON_SCHEMA.cstring, PERSON_SCHEMA.len-1, testSchema.addr)
 # echo jsonSchema
 # err = avroStrerror()
 # echo err
 
 
-proc initPersonSchema(): AvroSchema_t =
+proc initPersonSchema*(): AvroSchema_t =
     var res: int
     let
       id = avro_schema_long()
@@ -44,12 +46,10 @@ proc initPersonSchema(): AvroSchema_t =
     personSchema
 
 
-
-proc addPerson(schema: AvroSchemaT,
-               db: AvroFileWriterT,
+proc addPerson*(schema: AvroSchemaT,
                first, last, phone: cstring,
                age: int
-               ) =
+               ):AvroValueT =
   var res: int
 
   var
@@ -69,6 +69,7 @@ proc addPerson(schema: AvroSchemaT,
                             id_value.addr, count.addr) == 0:
     res = id_value.iface.setLong(id_value.iface, id_value.self, idCounter)
     idCounter = idCounter + 1
+
   # Set First Name
   if person.iface.getByName(person.iface, person.self, "First".cstring,
                             first_value.addr, count.addr) == 0:
@@ -77,25 +78,45 @@ proc addPerson(schema: AvroSchemaT,
   # Set Last Name
   if person.iface.getByName(person.iface, person.self, "Last".cstring,
                             last_value.addr, count.addr) == 0:
-    res = last_value.iface.setString(last_value.iface, last_value.self, last)
+    res = last_value.addr.iface.setString(last_value.iface, last_value.self, last)
 
   # Set Phone
   if person.iface.getByName(person.iface, person.self, "Phone".cstring,
                             phone_value.addr, count.addr) == 0:
-    res = phone_value.iface.setString(phone_value.iface, phone_value.self, last)
+    res = phone_value.iface.setString(phone_value.iface, phone_value.self, phone)
 
   # Set Age
   if person.iface.getByName(person.iface, person.self, "Age".cstring,
                             age_value.addr, count.addr) == 0:
     res = age_value.iface.setInt(age_value.iface, age_value.self, age.int32)
 
+  person
+
+proc addPersonToDB*(schema: AvroSchemaT,
+               db: PAvroFileWriterT,
+               first, last, phone: cstring,
+               age: int
+               ) =
+  var res: int
+
+  var
+    personClass = avroGenericClassFromSchema(schema)
+    person: AvroValueT
+    id_value: AvroValueT
+    first_value: AvroValueT
+    last_value: AvroValueT
+    age_value: AvroValueT
+    phone_value: AvroValueT
+    count: int
+
+  person = addPerson(schema, first, last, phone, age)
   res = avroFileWriterAppendValue(db, person.addr)
-    # echo "Fail"
+
 
 var personSchema = initPersonSchema();
 echo avroSchemaRecordSize(personSchema)
 var
-  db: AvroFileWriterT
+  db: PAvroFileWriterT
   dbname = "test.db"
   self: pointer
   count = 1
@@ -108,7 +129,16 @@ err = avroStrerror()
 echo err
 # let toJson = avroSchemaToJson(personSchema, db)
 echo "here"
+for i in 0..2:
+  addPersonToDB(personSchema, db, "Emil".cstring, "Ivanichkov".cstring,
+            "088655555".cstring, 22)
+  addPersonToDB(personSchema, db, "Viktor".cstring, "Ivanov".cstring,
+            "123456789".cstring, 22)
+  addPersonToDB(personSchema, db, "Ivan".cstring, "Blajec".cstring,
+            "0000000000".cstring, 22)
+  addPersonToDB(personSchema, db, "Travel".cstring, "Isacc".cstring,
+            "0".cstring, 22)
 
-# addPerson(personSchema, db, "Emil".cstring, "Ivanichkov".cstring,
-#           "088655555".cstring, 22)
-
+err = avroStrerror()
+echo err
+echo db.avroFileWriterFlush
